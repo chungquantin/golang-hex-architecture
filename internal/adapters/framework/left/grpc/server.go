fa192fc60d175c8c88f2c538311f8f5841a2c7d8
Chung Quan Tin <s3818487@rmit.edu.vn>
2021-05-30 17:03:06 +0700

    bring it all together to main.go

--- internal/adapters/framework/left/grpc/server.go
+++ internal/adapters/framework/left/grpc/server.go
@@ -1,1 +1,36 @@
 package grpc
+
+import (
+	"log"
+	"net"
+
+	"hex/internal/adapters/framework/left/grpc/pb"
+	"hex/internal/ports"
+
+	"google.golang.org/grpc"
+)
+
+type ServerAdapter struct {
+	api ports.APIPort
+}
+
+func NewServerAdapter(api ports.APIPort) *ServerAdapter  {
+	return &ServerAdapter{api: api}
+}
+
+func (grpca ServerAdapter) Run()  {
+	var err error
+
+	listen, err := net.Listen("tcp",":9000")
+	if err != nil {
+		log.Fatalf("failed to listen on port 9000: %v", err)
+	}
+
+	arithmeticServiceServer := grpca
+	grpcServer := grpc.NewServer()
+	pb.RegisterArithmeticServiceServer(grpcServer, arithmeticServiceServer)
+
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("failed to server gRPC server on port 9000: %v", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,43 @@
 package main
 
+import (
+	"hex/internal/adapters/core/app/api"
+	"hex/internal/adapters/core/arithmetic"
+	"hex/internal/adapters/framework/right/db"
+	"hex/internal/ports"
+	"log"
+	"os"
+
+	gRPC "hex/internal/adapters/framework/left/grpc"
+)
+
 func main() {
+	var err error
+	
 	// ports
-	// var core ports.ArithmeticPort
+	var dbAdapter ports.DbPort
+	var core ports.ArithmeticPort
+	var appAdapter ports.APIPort
+	var gRPCAdapter ports.GRPCPort
+
+
+	dbDriver := os.Getenv("DB_DRIVER");
+	dbName := os.Getenv("DB_NAME")
+
+	dbAdapter, err = db.NewDbAdapter(dbDriver, dbName);
+
+	if err != nil {
+		log.Fatalf("failed to initiate database connection %v", err)
+	}
+	
+	defer dbAdapter.CloseDbConnection();
+
+	core = arithmetic.NewArithmeticAdapter();
+
+	appAdapter = api.NewApiAdapter(dbAdapter, core);
+
+	gRPCAdapter = gRPC.NewServerAdapter(appAdapter);
+	
+	gRPCAdapter.Run();
+	
 }
